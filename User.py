d4213b5fbbeb74444424a73f2724dc452163888f
Dennis HG <dennishg32@gmail.com>
2020-11-22 14:18:23 +0200

    adds docstring

--- User.py
+++ User.py
@@ -2,6 +2,16 @@ class User:
   userList = []
   def __init__(self, fname, lname, username, pwd, pwd_conf):
     
+    """
+     _init_ methods that help us define properties for our new user object
+      Args:
+          fname: New user first name 
+          lname: New user last name 
+          username: new Username
+          pwd: New user password
+          pwd_conf: New user password confirmation 
+    """
+    
     self.first_name = fname
     self.last_name = lname
     self.username = username
