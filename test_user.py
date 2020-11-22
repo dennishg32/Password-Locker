70575473dd0c76a98be4d4b721c8b3578703298b
Dennis HG <dennishg32@gmail.com>
2020-11-22 14:02:32 +0200

    creates test_init function for initializing objects

--- test_user.py
+++ test_user.py
@@ -1,10 +1,25 @@
 import unittest
 from user import User
 
-class TestUser(unittest.Test):  
+class TestUser(unittest.TestCase):  
   
   def setUp(self):
     """
       set up method to run before when user intend to test each class
     """
-    self.newUser = User("Dennis", "HG", "deHg", "kgl6", "kgl6")
+    self.newUser = User(
+      "Dennis",
+      "HG",
+      "deHg",
+      "kgl6",
+      "kgl6")
+    
+  def test_init(self):
+    """
+      test_init test case for object for initials
+    """
+    self.assertEqual(self.newUser.fname, "Dennis")
+    self.assertEqual(self.newUser.lname, "HG")
+    self.assertEqual(self.newUser.username, "deHg")
+    self.assertEqual(self.newUser.pwd, "kgl6")
+    self.assertEqual(self.newUser.pwd_conf, "kgl6")
